90b6b71d4660b64c0801da5b7a6a7720b8f15d64
Victoria Blackburn <victoriablackburn@victorias-air.home>
2020-11-27 15:56:43 -0500

    updated file structure and added models

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+// Create Schema
+const UserSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String
+        },
+        date: {
+            type: Date,
+            default: Date.now
+        }
+    },
+    { strict: false }
+);
+
+module.exports = User = mongoose.model("users", UserSchema);
