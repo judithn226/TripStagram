8b04be308c1b206b214e4dc7f795b5f2771b40c1
Judith Nwagwu <Judith5nwagwu@gmail.com>
2020-11-27 12:48:39 -0800

    adding routes

---
+++ routes/api.js
@@ -0,0 +1,17 @@
+t User = require( '../models/User.js')
+const express = require( 'express')
+
+const router = express.Router()
+
+router.get('/users', (req, res) => {
+  User.find({}).then((users) => res.json(users))
+})
+
+router.post('/createuser', ({ body }, res) => {
+  console.log(body)
+  User.create(body)
+    .then((done) => res.json(done))
+    .catch((err) => res.status(400).json(err))
+})
+
+module.exports = router
