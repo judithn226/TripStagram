fa8e66c759e1e500d73102f777c9adc6662d75d1
Victoria Blackburn <victoriablackburn@victorias-air.home>
2020-12-02 21:25:22 -0500

    updated banner and search.js

---
+++ client/src/Components/Banner.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { Button } from "@material-ui/core";
+import Search from "./Search";
+import { useHistory } from "react-router-dom";
+
+function Banner() {
+  const history = useHistory();
+  const [showSearch, setShowSearch] = useState(false);
+
+  return (
+    <div className="banner">
+      <div className="banner_search">
+        {showSearch && <Search />}
+        <Button
+          onClick={() => setShowSearch(!showSearch)}
+          className="banner__searchButton"
+          variant="outlined"
+        >
+          {showSearch ? "Hide" : "Search Dates"}
+        </Button>
+      </div>
+      <div className="banner_info">
+        <h1> Let's Get Away </h1>
+        <Button onClick={() => history.push("/search")} variant="outlined">
+          Plan Your Stay
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
