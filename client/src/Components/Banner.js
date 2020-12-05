6537b15f91b84d18eca43e6affee1307c7cb01eb
Monika Kakuturu <monikakakuturu@gmail.com>
2020-12-04 23:45:11 -0800

    re arranged the home page controls

--- client/src/Components/Banner.js
+++ client/src/Components/Banner.js
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 import { Button } from "@material-ui/core";
 import Search from "./Search";
 import { useHistory } from "react-router-dom";
+import './Banner.css'
+import TravelBackground from "../Assets/images/BeachBackground.jpg"
+import { Container } from '@material-ui/core';
+import Carousel from 'react-bootstrap/esm/Carousel';
 
 function Banner() {
   const history = useHistory();
@@ -9,22 +13,33 @@ function Banner() {
 
   return (
     <div className="banner">
+      <Container>
+        <Carousel className="carousel-inner">
+          <Carousel.Item className="item">
+            <img className= "d-block w-100"
+            src={TravelBackground}
+            alt="Beach background">
+            </img>
+            <Carousel.Caption>
+                <h1>Let's Get Away</h1>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </Container>
+      
       <div className="banner_search">
         {showSearch && <Search />}
-        <Button
+        <Button 
           onClick={() => setShowSearch(!showSearch)}
           className="banner__searchButton"
-          variant="outlined"
-        >
+          variant="outlined">
           {showSearch ? "Hide" : "Search Dates"}
         </Button>
-      </div>
-      <div className="banner_info">
-        <h1> Let's Get Away </h1>
         <Button onClick={() => history.push("/search")} variant="outlined">
           Plan Your Stay
         </Button>
       </div>
+      
     </div>
   );
 }
