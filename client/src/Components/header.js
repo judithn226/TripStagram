9991b4f51af3304a6f197d37017cde80f252b4a7
usmanjchaudhry <36524989+usmanjchaudhry@users.noreply.github.com>
2020-12-12 13:48:00 -0800

    you know how it goes

--- client/src/Components/header.js
+++ client/src/Components/header.js
@@ -1,53 +1,31 @@
-import React from "react";
-import './header.css';
-// import logo from "../Assets/images/Trippin";
+import React from 'react'
+import './header.css'
+import LOGO from "../Assets/images/trippinLogo.JPG"
+import { Link } from "react-router-dom";
+
 
 function Header() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark">
-      <a className="navbar-brand" href="/">
-        {/* <img className="navbar-logo" src={logo} alt=""></img>       */}
-        Trippin'
-      </a>
-      <button
-        className="navbar-toggler"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon" />
-      </button>
+    <div className='header'>
+        <Link to='/'>
+            <img
+                className="header__icon"
+                src={LOGO}
+                alt=""
+            />
+        </Link>
+       
+        <div className='header__center'>
+            <input type="text" placeholder = "Type to search" />
+       
+        </div>
 
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            {/* <a className="nav-link" href="/">
-              Home <span className="sr-only">(current)</span>
-            </a> */}
-          </li>
-        </ul>
-        <ul className="navbar-nav">
-        <li className="nav-item active">
-            <a className="nav-link" href="signup">
-              SignUp
-            </a>
-          </li>
-          <li className="nav-item active">
-            <a className="nav-link" href="login">
-              LogIn
-            </a>
-          </li>
-          <li className="nav-item active">
-            <a className="nav-link" href="places">
-              Places
-            </a>
-          </li>
-          </ul>
-      </div>
-    </nav>
-  );
+        <div className='header__right'>
+            <p>LogIn</p>
+            <p>Signup</p>
+        </div>
+    </div>
+)
 }
 
 export default Header;
