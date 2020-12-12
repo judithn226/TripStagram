4d162dcd0b38edfc85219523ec9cfcd56fe0c9f5
Victoria Blackburn <victoriablackburn@victorias-air.home>
2020-12-12 13:20:04 -0500

    updated home.jss

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,40 +1,22 @@
 import React from 'react';
-import './home.css';
-import Banner from './banner'
+import './Home.css';
+import Banner from './Banner'
+import Card from './Card'
 import { Container } from '@material-ui/core';
 import Carousel from 'react-bootstrap/esm/Carousel';
-import Slider1 from "../Assets/images/Slider1.jpg"
-import Slider2 from "../Assets/images/Slider2.jpg"
-import Slider3 from "../Assets/images/Slider3.jpg"
-import axios from 'axios'
+import OnlineExperiences from "../Assets/images/Travelbackground.jpg"
 
 
 
 function Home() {
     return (
         <div className='home'>
-            <Container>
             <Banner />
-            </Container>
-            
-
-            {/* <div className='home__section'>
             <Container>
                 <Carousel>
-                    <Carousel.Item className="slideitem" interval={1500}>
+                    <Carousel.Item className="slideitem" interval={2000}>
                         <img className= "d-block w-100"
-                        src={Slider1}
-                        alt="Travel Plans">
-                        </img>
-                        <Carousel.Caption>
-                            <h3>Travel Plans</h3>
-                            <p>Plan the travel together, go places you like.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-
-                    <Carousel.Item className="slideitem" interval={1500}>
-                        <img className= "d-block w-100"
-                        src={Slider3}
+                        src={OnlineExperiences}
                         alt="Online Experiences">
                         </img>
                         <Carousel.Caption>
@@ -43,7 +25,7 @@ function Home() {
                         </Carousel.Caption>
                     </Carousel.Item>
 
-                    <Carousel.Item className="slideitem" interval={1500}>
+                    <Carousel.Item className="slideitem" interval={2000}>
                         <img className= "d-block w-100"
                         src="https://a0.muscache.com/im/pictures/15159c9c-9cf1-400e-b809-4e13f286fa38.jpg"
                         alt="Unique Stays">
@@ -54,7 +36,7 @@ function Home() {
                         </Carousel.Caption>
                     </Carousel.Item>
 
-                    <Carousel.Item className="slideitem" interval={1500}>
+                    <Carousel.Item className="slideitem" interval={2000}>
                         <img className= "d-block w-100"
                         src="https://a0.muscache.com/im/pictures/fdb46962-10c1-45fc-a228-d0b055411448.jpg"
                         alt="Entire Homes">
@@ -66,17 +48,15 @@ function Home() {
                     </Carousel.Item>
                     
                 </Carousel>
-            </Container>             */}
+            </Container>            
         </div>
     )
-};
-
-
+}
 
 //Below I inserted an axios call to call on the API. You can either keep it here
 //or put this in the places component. Either way, you need to create a search bar
-// to pass in a query for this call. Dropdown might work too. */}
-{/* const options = {
+// to pass in a query for this call. Dropdown might work too.
+const options = {
   method: 'GET',
   url: 'https://tripadvisor1.p.rapidapi.com/airports/search',
   params: {query: 'new york', locale: 'en_US'},
@@ -89,6 +69,6 @@ axios.request(options).then(function (response) {
 	console.log(response.data);
 }).catch(function (error) {
 	console.error(error);
-}); */}
+});
 
 export default Home;
