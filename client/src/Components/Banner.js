19a3533bb99ecff8940e896c2712418eb02ea1f2
usmanjchaudhry <36524989+usmanjchaudhry@users.noreply.github.com>
2020-12-17 15:08:45 -0800

    added planning

--- client/src/Components/Banner.js
+++ client/src/Components/Banner.js
@@ -1,181 +1,98 @@
 import React, { useState, useRef } from "react";
 import { Button } from "@material-ui/core";
-import Search from "./searchResult";
+import Search from "./Search";
 import { useHistory } from "react-router-dom";
-import './Banner.css'
-import Bali from "../Assets/images/Bali.jpg"
-import { Container } from '@material-ui/core';
-import axios from 'axios';
+import "./Banner.css";
+import Bali from "../Assets/images/Bali.jpg";
+import { Container } from "@material-ui/core";
+import axios from "axios";
 import { set } from "date-fns";
 import SearchResult from "./searchResult";
 
 
+
 function Banner() {
-  
-  const history = useHistory();
-  const [showSearch, setShowSearch] = useState(false);
-  const [showResults, setResults] = useState([])
-  const inputField = useRef()
-
-  function search(){
-    const city = inputField.current.value
 
 
-
- 
-
-    const options = {
-      method: 'GET',
-      url: 'https://hotels-com-free.p.rapidapi.com/suggest/v1.7/json',
-      params: {query: city, locale: 'en_US'},
-      headers: {
-        'x-rapidapi-key': '9f05d26673mshb2c8499547bc2c5p1c9aafjsn197659763efe',
-        'x-rapidapi-host': 'hotels-com-free.p.rapidapi.com'
-      }
-    };
+  // var word=document.getElementById("searchBar").value
     
-    axios.request(options).then(function (response) {
+  
+
+
+  const options = {
+    method: "GET",
+    url: "https://hotels4.p.rapidapi.com/locations/search",
+    params: { query: "new york" , locale: "en_US" },
+    headers: {
+      "x-rapidapi-key": "9f05d26673mshb2c8499547bc2c5p1c9aafjsn197659763efe",
+      "x-rapidapi-host": "hotels4.p.rapidapi.com",
+    },
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
       console.log(response.data);
-    }).catch(function (error) {
+      console.log(response.data.suggestions[3].entities);
+     
+    })
+    .catch(function (error) {
       console.error(error);
     });
 
-
-    // const options = {
-    //   method: 'GET',
-    //   url: 'https://hotels4.p.rapidapi.com/locations/search',
-    //   params: {query: city, locale: 'en_US'},
-    //   headers: {
-    //     'x-rapidapi-key': '9f05d26673mshb2c8499547bc2c5p1c9aafjsn197659763efe',
-    //     'x-rapidapi-host': 'hotels4.p.rapidapi.com'
-    //   }
-    // };
-    
-    // axios.request(options).then(function (response) {
-    //   console.log(response.data);
-    //   console.log(response.data.suggestions)
-    //  setResults(response.data.suggestions)
-    // }).catch(function (error) {
-    //   console.error(error);
-    // });
+    const history = useHistory();
+    const [showSearch, setShowSearch] = useState(false);
+    const [showResults, setResults] = useState([])
+    const inputField = useRef()
+  
+   
 
 
-    // const options = {
-    //   method: 'GET',
-    //   url: 'https://tripadvisor1.p.rapidapi.com/airports/search',
-    //   params: {query: city, location: 'en_US'},
-    //   headers: {
-    //     'x-rapidapi-key': '9f05d26673mshb2c8499547bc2c5p1c9aafjsn197659763efe',
-    //     'x-rapidapi-host': 'tripadvisor1.p.rapidapi.com'
-    //   }
-    // };
-    // axios.request(options).then(function (response) {
-    //   console.log(response.data);
-    //   setResults(response.data)
-    // }).catch(function (error) {
-    //   console.error(error);
-    // });
-  }
-  // function Plan(){
-  //   //redirect with
-    
-  //   console.log("searching")
-  //   // <Link to ="/searchResult">SearchResult</Link>
-  //   const options = {
-  //     method: 'GET',
-  //     url: 'https://tripadvisor1.p.rapidapi.com/airports/search',
-  //     params: {query: 'los angeles', location: 'en_US'},
-  //     headers: {
-  //       'x-rapidapi-key': '9f05d26673mshb2c8499547bc2c5p1c9aafjsn197659763efe',
-  //       'x-rapidapi-host': 'tripadvisor1.p.rapidapi.com'
-  //     }
-  //   };
-  //   axios.request(options).then(function (response) {
-  //     console.log(response.data);
-  //     //redirect to searchresultpage
-  //     //pass the data into context or Redirect and pass data
-  //   }).catch(function (error) {
-  //     console.error(error);
-  //   });
-  // //   const options = {
-  // //     method: 'GET',
-  // //     url: 'https://airbnb-com.p.rapidapi.com/listings/nearby/37.788719679657554/-122.40057774847898',
-  // //     params: {
-  // //       offset: '0',
-  // //       locale: 'en-US',
-  // //       min_price: '0',
-  // //       max_price: '5000',
-  // //       //change guests belowe to a variable you are passing in.
-  // //       max_guests: '2',
-  // //       //change check_in and checkout date too
-  // //       check_in: '2021-02-25',
-  // //       radius: '5',
-  // //       currency: 'USD',
-  // //       check_out: '2021-02-26'
-  // //     },
-  // //     headers: {
-  // //       'x-rapidapi-key': '9f05d26673mshb2c8499547bc2c5p1c9aafjsn197659763efe',
-  // //       'x-rapidapi-host': '-com.p.rapidapi.com'
-  // //     }
-  // //   };
-    
-    
-  // //     axios.request(options).then(function (response) {
-  // //       console.log(response.data);
-  // //     }).catch(function (error) {
-  // //       console.error(error);
-  // //     });
 
-
-  // }
 
   return (
     <div className="banner">
-      {/* <div className='banner_center'>
-                <input type="text" 
-                ref={inputField}
-                name = "searchBar"
-                id = "searchBar"
-                placeholder = "Type to search" />
-            </div>
-            <ul id="charactersList"></ul> */}
-      
+      {/* <div className='banner__search1'>
+                {showSearch && <Search />}
+ 
+                <Button onClick={() => setShowSearch(!showSearch)}  class="button1" className='banner__searchButton' variant='outlined'>
+                    {showSearch ? "Hide" : "Search Dates"}
+                </Button>
+            </div> */}
       <div className="banner_search">
         {showSearch && <Search />}
+       
         <a href = "searchResult">
-        <Button class = "button"
-         
-         
+        
+        <Button
+          class="button"
           className="banner__searchButton"
-          variant="outlined">
+          variant="outlined"
+        >
           {showSearch ? "Hide" : "Start Trippin"}
         </Button>
         </a>
-      
-        <div className='banner_center'>
-                <input type="text" 
-                ref={inputField}
-                name = "searchBar"
-                id = "searchBar"
-                placeholder = "Where you wanna trip too?" />
-            </div>
-      </div>
-      {
         
-        showResults.map(item=> {
-          return(
-            <h1>{item.display_name}</h1>
-          )
-        })
-    
-    }
-    <div
-    searchData = {
-      showResults
-     }/>
-    
-    </div>
 
+        <div className="banner_center">
+          <input
+            type="text"
+            ref={inputField}
+            name="searchBar"
+            id="searchBar"
+            // value="new york"
+            placeholder="Where you wanna trip too?"
+          />
+        </div>
+        <Button onClick={() => setShowSearch(!showSearch)}  class="button1" className='banner__searchButton' variant='outlined'>
+                    {showSearch ? "Hide" : "Search Dates"}
+         </Button>
+      </div>
+      {showResults.map((item) => {
+        return <h1>{item.display_name}</h1>;
+      })}
+      <div searchData={showResults} />
+    </div>
   );
 }
 
